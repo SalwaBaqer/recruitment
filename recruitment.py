c0a865b938670bfc8736d2aad29a5ea0261e8811
SalwaBaqer <s.abaqer@hotmail.com>
2022-04-11 17:55:30 +0300

    Solve the task

--- recruitment.py
+++ recruitment.py
@@ -1,36 +1,41 @@
 def get_skills():
-    # Add at least 3 random skills for the user to select from
-    return []
-
+    return ["Python","C++", "Javascript", "Juggling", "Running", "Eating"]
 
 def show_skills(skills):
-    # Pretty print skills to the user
-    # Write your code here
-    ...
-
+    print()
+    for count,skill in enumerate(skills,1):
+     print (f"{count}. {skill}")
 
 def get_user_skills(skills):
-    # Show the available skills and have user pick from them
-    # Write your code here
-    ...
-
+   show_skills(skills)
+   chosen_skills= [input("\nChoose a skill from above by entering its number:"),input("Choose a skill from above by entering its number:")]
+   return [skills[int(skill)-1] for skill in chosen_skills]
 
 def get_user_cv(skills):
-    # Get the users CV from their inputs
-    # Write your code here
-    ...
-
-
+    return{
+         "name" : input("What's your name? "),
+         "age" : int(input("How old are you? ")),
+         "experience" : int(input("How many years of experience do you have? ")),
+         "skills" : get_user_skills(skills)
+}
+  
 def check_acceptance(cv, desired_skill):
-    # Check if the cv is acceptable or not and return a boolean based on that
-    # Write your code here
-    ...
-
-
+    return (cv["age"] >= 25 
+    and cv["age"] <= 40 and cv["experience"] > 3 
+    and desired_skill in cv["skills"])
+    
 def main():
-    # Write your main logic here by combining the functions above into the
-    # desired outcome
-    ...
+   print("Welcome to the special recruitment program, please answer the following questions:")
+   
+   skills = get_skills()
+   desired_skill = 2
+
+   cv = get_user_cv(skills)
+  
+   if(check_acceptance(cv, skills[desired_skill])):
+       print(f"Congratulation...you have been accepted, {cv['name']}.")
+   else:
+       print(f"Sorry...you have been rejected, {cv['name']}.")
 
 
 if __name__ == "__main__":
